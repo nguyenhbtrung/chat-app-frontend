ed1b332f353b5e63d38956447c025653a6b3505f
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2025-08-01 16:26:59 +0700

    configure i18n and add API error messages (en, vi)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './i18n'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Suspense fallback={<div>Loading page...</div>}>
+      <App />
+    </Suspense>
   </StrictMode>,
 )
