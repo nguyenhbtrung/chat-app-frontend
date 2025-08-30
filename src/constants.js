21fc9c757e3eb64e14ad426866df808de298b337
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2025-08-30 13:15:37 +0700

    add THEMES, LANGUAGES enum-like constants

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,11 @@
 export const ACCESS_TOKEN_KEY_NAME = 'accessToken';
+
+export const THEMES = {
+    LIGHT: "light",
+    DARK: "dark",
+};
+
+export const LANGUAGES = {
+    EN: "en",
+    VI: "vi",
+};
